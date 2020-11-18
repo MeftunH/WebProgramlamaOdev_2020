9451625c73c91ba9f4eb88f28fb638eab681fd58
erendgrmnc <57503158+erendgrmnc@users.noreply.github.com>
2020-11-18 21:02:39 +0300

    1-)Veri Tabanı Modeli Oluşturuldu. 2-)Modele Göre Sınıflar Oluşturuldu. 3-)Veri Tabanı Migrationı yapıldı.

---
+++ WebProgramlamaOdev/Models/PokemonStat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProgramlamaOdev.Models
+{
+    public class PokemonStat
+    {
+        [Key]
+        public int POKEMON_STAT_ID { get; set; }
+        public ICollection<Pokemon> POKEMON_ID { get; set; }
+        public ICollection<Stat> STAT_ID { get; set; }
+        public int STAT_VALUE { get; set; }
+    }
+}
---
+++ WebProgramlamaOdev/Models/UserWishlist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProgramlamaOdev.Models
+{
+    public class UserWishlist
+    {
+        [Key]
+        public int USER_WISH_ID { get; set; }
+        public ICollection<Pokemon> POKEMON_ID { get; set; }
+        public ICollection<User> USER_ID { get; set; }
+        public DateTime CREATION_DATE { get; set; }
+    }
+}
