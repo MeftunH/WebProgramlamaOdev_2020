ae9d60367885b5a9e1e0e43c0bb899d319f530ba
erendgrmnc <57503158+erendgrmnc@users.noreply.github.com>
2021-01-02 17:59:15 +0300

    Profil Sayfası için gerekli geliştirmelere başlandı.

---
+++ WebProje/WebProje/ViewComponents/Profile/Pokemons.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebProje.Models;
+
+namespace WebProje.ViewComponents.Profile
+{
+    public class Pokemons:ViewComponent
+    {
+        Context context = new Context();
+
+        public IViewComponentResult Invoke(string userid)
+        {
+            var userPokemons = context.USER_POKEMON.Where(up => up.Id == userid).ToList();
+
+            List<Models.Pokemon> pokemons = new List<Models.Pokemon>();
+
+            foreach (var up in userPokemons)
+            {
+                pokemons.Add(context.POKEMON.Where(p => p.POKEMON_ID == up.POKEMON_ID).FirstOrDefault());
+            }
+
+            return View(pokemons);
+
+        }
+    }
+}
