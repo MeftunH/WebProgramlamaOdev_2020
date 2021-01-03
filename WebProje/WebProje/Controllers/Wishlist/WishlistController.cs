02f4e3011464050e1d16d1bd1107cf871491b75f
MeftunH <maftunhashimli@gmail.com>
2021-01-03 05:38:45 +0400

    wishlist ve satin aldiklarimda kucuk duzenlemeler

---
+++ WebProje/WebProje/Controllers/Wishlist/WishlistController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebProje.Models;
+
+namespace WebProje.Controllers.Wishlist
+{
+    public class WishlistController : Controller
+    {
+        Context context = new Context();
+        public IActionResult Index()
+        {
+            Models.User user = context.USER.Where(u => u.Email == User.Identity.Name).FirstOrDefault();
+
+            return View(user);
+        }
+    }
+}
---
+++ WebProje/WebProje/ViewComponents/Wishlist/Pokemon.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebProje.Models;
+
+namespace WebProje.ViewComponents.Wishlist
+{
+    public class Pokemons : ViewComponent
+    {
+        Context context = new Context();
+
+        public IViewComponentResult Invoke(string userid)
+        {
+            var userWishlist = context.USER_WISHLIST.Where(uw => uw.Id == userid).ToList();
+            List<Models.Pokemon> pokemons = new List<Models.Pokemon>();
+
+            foreach (var wish in userWishlist)
+            {
+                pokemons.Add(context.POKEMON.Where(p => p.POKEMON_ID == wish.POKEMON_ID).FirstOrDefault());
+            }
+
+            return View(pokemons);
+        }
+    }
+}
